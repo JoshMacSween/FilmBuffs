e7fb2356ae6b643ca62ad42419bc3f863b741efa
Joshua MacSween <joshmacsween@gmail.com>
2020-11-02 21:53:13 -0700

    working axios GET request

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,16 @@ const http = require('http')
 
 const app = express()
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('Backend Running')
 })
 
+app.get(
+  `/http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=Sharknado&y=2013&plot=short&r=xml`,
+  (req, res) => {
+    res.send(req.body)
+  }
+)
 
 const port = process.env.PORT || 5000
 
--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Container, Col, Row, Card } from 'react-bootstrap'
 
 export default function MainContent() {
+  const [q, setQ] = useState('Blade Runner')
+
+  useEffect(() => {
+    axios
+      .get(`http://www.omdbapi.com/?s=${q}&apikey=${process.env.REACT_APP_API_KEY}`)
+      .then(
+        (response) => {
+          console.log(response)
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+  })
+
   return (
     <div>
       <Container>
         <h1 className="pt-3">Welcome to FilmBuffs</h1>
         <i>We know movies</i>
-          <Row>
-            <Col><Card>Just type in the movie you'd like to learn more about</Card></Col>
-            <Col><Card>And we'll do the rest</Card></Col>
-          </Row>
+        <Row>
+          <Col>
+            <Card>Just type in the movie you'd like to learn more about</Card>
+          </Col>
+          <Col>
+            <Card>And we'll do the rest</Card>
+          </Col>
+        </Row>
       </Container>
     </div>
   )
