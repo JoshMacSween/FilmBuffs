e2d837b9cf3a94ad139e6347f9a003a1ba7dbe12
Joshua MacSween <joshmacsween@gmail.com>
2020-11-06 18:35:52 -0700

    basic sign in page

--- frontend/src/screens/SignUp.js
+++ frontend/src/screens/SignUp.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import {Container, Row, Col, Button, Form} from 'react-bootstrap'
+import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 
 export default function SignUp() {
   return (
     <Container>
-      <Row>
-        <Col>
-          Sign up!
-        </Col>
-      </Row>
+      <Form className="py-3">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>User Name</Form.Label>
+          <Form.Control type="text" placeholder="Choose a name" />
+          <Form.Label className="py-3">Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </Container>
   )
 }
