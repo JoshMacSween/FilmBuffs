52d1524eae075febd6d9586bdbc1b7d6ec21c57c
Joshua MacSween <joshmacsween@gmail.com>
2020-11-15 12:13:51 -0700

    added image to readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # FilmBuffs
 Using the OMDb API, the user searches for a film by title and is presented data fetched from the API.
 
-
+![Home Page](https://github.com/JoshMacSween/FilmBuffs/blob/master/frontend%2Fpublic%2FfilmBuffs_XMen.png)
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const http = require('http')
+// const express = require('express')
+// const dotenv = require('dotenv')
+// const http = require('http')
 
-const app = express()
+// const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Backend Running')
-})
+// app.get('/', (req, res) => {
+//   res.send('Backend Running')
+// })
 
-app.get(
-  `/http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=Sharknado&y=2013&plot=short&r=xml`,
-  (req, res) => {
-    res.send(req.body)
-  }
-)
+// app.get(
+//   `/http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=Sharknado&y=2013&plot=short&r=xml`,
+//   (req, res) => {
+//     res.send(req.body)
+//   }
+// )
 
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
 
-app.listen(port, console.log(`Server listening at port: ${port}`))
+// app.listen(port, console.log(`Server listening at port: ${port}`))
