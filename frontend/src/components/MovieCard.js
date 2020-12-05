d73308effa1b2bcaa12a437e67ccc0ef6df25a88
Joshua MacSween <joshmacsween@gmail.com>
2020-12-05 16:03:49 -0700

    removed card from around movie poster

--- frontend/src/components/MovieCard.js
+++ frontend/src/components/MovieCard.js
@@ -5,23 +5,21 @@ import { Link } from 'react-router-dom'
 export default function MovieCard({ id, year, title, poster, plot }) {
   return (
     <>
-      <Card>
-        <Card.Body>
-          <Link
-            to={{
-              pathname: `/search/${id}`,
-              state: {
-                title: `${title}`,
-                year: `${year}`,
-                poster: `${poster}`,
-                plot: `${plot}`,
-              },
-            }}
-          >
-            <Card.Img src={poster} />
-          </Link>
-        </Card.Body>
-      </Card>
+
+        <Link
+          to={{
+            pathname: `/search/${id}`,
+            state: {
+              title: `${title}`,
+              year: `${year}`,
+              poster: `${poster}`,
+              plot: `${plot}`,
+            },
+          }}
+        >
+          <Card.Img className="moviePoster pb-3" src={poster} />
+        </Link>
+
     </>
   )
 }
