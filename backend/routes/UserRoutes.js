151f08adcc4d03eac330a1dabf834491d847e64e
Joshua MacSween <joshmacsween@gmail.com>
2020-12-08 17:09:38 -0700

    deleting users

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const auth = require('../middleware/auth')
 const User = require('../models/UserModel')
 
 router.post('/register', async (req, res) => {
@@ -54,8 +55,28 @@ router.post('/login', async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' })
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+    res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        userName: user.userName,
+      },
+    })
+    console.log(token)
   } catch (err) {
     res.status(500).json({ error: err.message })
+  }
+})
+
+router.delete('/delete', auth, async (req, res) => {
+  try {
+    const userDelete = await User.findByIdAndDelete(req.user)
+    res.json(userDelete)
+  } catch (err) {
+    res.status(500).json({ msg: err.message })
   }
 })
 
