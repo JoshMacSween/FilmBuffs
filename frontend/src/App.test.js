c87154ecf607861689b13f01ccfcc72e44be880c
Joshua MacSween <joshmacsween@gmail.com>
2021-01-25 11:36:37 -0700

    rudimentary tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import { shallow } from "enzyme";
+import App from "./App";
+import MainContent from "./components/MainContent";
+
+describe("Main Content Tests", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<MainContent />);
+  });
+  test("Renders welcome", () => {
+    expect(wrapper.find("h1").text()).toContain("Welcome to FilmBuffs");
+  });
 });
